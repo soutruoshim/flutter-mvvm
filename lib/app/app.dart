01a7fa8edf3df021600fe5c3973a876bcfddb5a3
Sout Rahim <sout.rahim@gmail.com>
2024-01-03 22:09:14 +0700

    theme Manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/theme_manager.dart';
+
 class MyApp extends StatefulWidget {
 
   MyApp._internal(); // private named constructor
@@ -15,6 +17,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
   }
 }
