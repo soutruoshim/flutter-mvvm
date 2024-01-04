185712ef84f1ee37099a11e61369b1e07431a3be
Sout Rahim <sout.rahim@gmail.com>
2024-01-04 22:01:16 +0700

    appbar theme, button theme and card theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'color_manager.dart';
+import 'font_manager.dart';
+import 'styles_manager.dart';
+import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
@@ -9,13 +12,33 @@ ThemeData getApplicationTheme() {
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager
           .grey1, // will be used incase of disabled button for example
-      accentColor: ColorManager.grey
+      accentColor: ColorManager.grey,
     // card view theme
-
+      cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4),
     // App bar theme
-
+      appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
     // Button theme
+      buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70),
 
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: getRegularStyle(color: ColorManager.white),
+            primary: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(AppSize.s12)))),
     // Text theme
 
     // input decoration theme (text form field)
